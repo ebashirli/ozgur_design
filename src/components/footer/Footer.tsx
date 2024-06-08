482fb13d3500f35d7c486dfa490b5f4b8b594ce5
Elvin Bashirli <ebashirli@gmail.com>
2024-06-08 20:26:20 +0400

    implementation of initial real data

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="primary px-[7.778%] bg-primary text-white py-6">
       <p className="text-right">
-        &copy; Ozgur Design Ltd. ©{new Date().getFullYear()}. Bütün hüquqları
+        Ozgur Design Ltd. &copy; {new Date().getFullYear()}. Bütün hüquqları
         qorunur.
       </p>
       {/* <p>Develeped by: </p> */}
